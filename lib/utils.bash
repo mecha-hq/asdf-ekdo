d814b49f4a3d22a50a310601cafb80ecbfb99b7a
Claudio Beatrice <claudi0.beatric3@gmail.com>
2024-11-08 18:07:07 +0100

    fix: remove broken flag from untar command

--- lib/utils.bash
+++ lib/utils.bash
@@ -7,7 +7,7 @@ TOOL_NAME="ekdo"
 TOOL_TEST="ekdo --help"
 
 fail() {
-	echo -e "asdf-$TOOL_NAME: $*"
+	echo -e "asdf-${TOOL_NAME}: $*"
 	exit 1
 }
 
@@ -49,22 +49,22 @@ install_version() {
 	local version="$2"
 	local install_path="${3%/bin}/bin"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
+	if [ "${install_type}" != "version" ]; then
+		fail "asdf-${TOOL_NAME} supports release installs only"
 	fi
 
 	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		mkdir -p "${install_path}"
+		cp -r "${ASDF_DOWNLOAD_PATH}"/* "${install_path}"
 
 		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		tool_cmd="$(echo "${TOOL_TEST}" | cut -d' ' -f1)"
+	 	test -x "${install_path}/$tool_cmd" || fail "Expected ${install_path}/$tool_cmd to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
+	 	echo "${TOOL_NAME} ${version} installation was successful!"
 	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
+		rm -rf "${install_path}"
+		fail "An error occurred while installing ${TOOL_NAME} ${version}."
 	)
 }
 
