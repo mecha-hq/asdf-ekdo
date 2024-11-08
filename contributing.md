d56b417a994fb3d5eadbcfa7052672ccdbeba94d
Claudio Beatrice <claudi0.beatric3@gmail.com>
2024-11-08 16:16:10 +0100

    feature: configure the basic ekdo asdf plugin

--- contributing.md
+++ contributing.md
@@ -3,9 +3,6 @@
 Testing Locally:
 
 ```shell
-asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
-
-# TODO: adapt this
 asdf plugin test ekdo https://github.com/mecha-hq/asdf-ekdo.git "ekdo --help"
 ```
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for ekdo.
 GH_REPO="https://github.com/mecha-hq/ekdo"
 TOOL_NAME="ekdo"
 TOOL_TEST="ekdo --help"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if ekdo has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,7 +38,6 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for ekdo
 	url="$GH_REPO/archive/v${version}.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
@@ -61,7 +57,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert ekdo executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
